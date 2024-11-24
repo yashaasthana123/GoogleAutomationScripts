9df312ec6d1a78b08fe211888fa1fa4d5c41e624
yashaasthana123 <yasha.asthana@gmail.com>
2024-11-24 17:59:29 -0600

    Update GoogleSearchClass.java

--- GoogleSearchClass.java
+++ GoogleSearchClass.java
@@ -11,13 +11,27 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class GoogleSearchClass {
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		WebDriver driver = new ChromeDriver(); // Open the Browser
 		driver.get("https://www.google.com");
 		
+		//Stay Sign Out button
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		System.out.println(driver.findElement(By.xpath("//div[@class='hXhhq']//div[text()='Sign in to Google']") ).getText());
+	
+		//System.out.println(driver.findElement(By.xpath("//*[@aria-label='Stay signed out']")).getText());
+		
+		/*
+		// About Tab
 		System.out.println(driver.findElement(By.className("MV3Tnb")).getText());
-		driver.findElement(By.className("MV3Tnb")).click();
-		System.out.println(driver.getTitle());
-		driver.navigate(). back();
+		if((driver.findElement(By.className("MV3Tnb"))).isDisplayed())
+		{
+			driver.findElement(By.className("MV3Tnb")).click();
+			System.out.println(driver.getTitle());
+			driver.navigate(). back();
+		}else
+			{System.out.println("Element not found");}
+			
 	    
 		System.out.println("Browser Title: "+ driver.getTitle());
 		System.out.println("URL : "+ driver.getCurrentUrl());
@@ -32,12 +46,8 @@ public class GoogleSearchClass {
 		System.out.println("You have Searched: "+driver.getTitle());
 		System.out.println("Your current Searched url: "+driver.getCurrentUrl());
 		
-		
-
-		Thread.sleep(5000);	
-		
-		driver.quit();
-
+		//driver.quit();
+*/
 	}
 
 }
