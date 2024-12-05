2ec8c178853c779b36dffe8024953812a72ba7ae
yashaasthana123 <yasha.asthana@gmail.com>
2024-12-04 22:16:37 -0600

    Update GoogleSearchClass.java
    "Stay Logout" iframe updated

--- GoogleSearchClass.java
+++ GoogleSearchClass.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,12 +17,14 @@ public class GoogleSearchClass {
 		driver.get("https://www.google.com");
 		
 		//Stay Sign Out button
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		System.out.println(driver.findElement(By.xpath("//div[@class='hXhhq']//div[text()='Sign in to Google']") ).getText());
-	
-		//System.out.println(driver.findElement(By.xpath("//*[@aria-label='Stay signed out']")).getText());
 		
-		/*
+		WebElement iframeresult= driver.findElement(By.xpath("//*[@id=\"gb\"]//div[3]/iframe"));
+		driver.switchTo().frame(iframeresult);
+		System.out.println(driver.findElement(By.xpath("//button[@aria-label='Stay signed out']")).getText());
+		driver.findElement(By.xpath("//button[@aria-label='Stay signed out']")).click();
+		driver.switchTo().defaultContent();
+		
+		
 		// About Tab
 		System.out.println(driver.findElement(By.className("MV3Tnb")).getText());
 		if((driver.findElement(By.className("MV3Tnb"))).isDisplayed())
@@ -37,17 +40,17 @@ public class GoogleSearchClass {
 		System.out.println("URL : "+ driver.getCurrentUrl());
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
 		
-		//driver.findElement(By.xpath("//*[@aria-label=\"Stay signed out\"]")).click();
-		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
+	
 		
 		driver.findElement(By.id("APjFqb")).sendKeys("Selenium");
 		driver.findElement(By.xpath("//*[@id=\"jZ2SBf\"]/div[1]/span")).click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
 		System.out.println("You have Searched: "+driver.getTitle());
 		System.out.println("Your current Searched url: "+driver.getCurrentUrl());
+		driver.navigate(). back();
 		
-		//driver.quit();
-*/
+		driver.quit();
+
 	}
 
 }
